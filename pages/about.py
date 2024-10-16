51f8e9d8144a3dca61ff475c37a918708a5bb451
Erty Seidohl <eseidohl@google.com>
2024-10-16 01:20:06 +0000

    Add tests, login, format everything

--- pages/about.py
+++ pages/about.py
@@ -3,11 +3,14 @@ import streamlit as st
 if st.session_state.get("logged_in") == None:
     st.session_state["logged_in"] = False
 
+
 def login():
     st.session_state.logged_in = True
 
+
 def logout():
     st.session_state.logged_in = False
+
 
 st.set_page_config(page_title="About")
 
@@ -16,10 +19,10 @@ st.sidebar.header("About")
 
 if st.session_state.logged_in:
     st.sidebar.success("Logged in")
-    st.sidebar.button("Log out", on_click=logout)
+    st.sidebar.button("Log out", key="logout", on_click=logout)
 else:
     st.sidebar.warning("Not logged in")
-    st.sidebar.button("Log in", on_click=login)
+    st.sidebar.button("Log in", key="login", on_click=login)
 
 st.sidebar.write("This site is copyright Fake Company LLC Inc., 2024")
 
@@ -29,7 +32,7 @@ st.markdown(
 
     It produces nothing and has $0 a year in revenue.
 
-    Usually, companies are not called both "LLC" and "Inc." and must choose one, but this is a fake company so it has both just to be funny.    
+    Usually, companies are not called both "LLC" and "Inc." and must choose one, but this is a fake company so it has both just to be funny.
 
     Here is a logo that I created with Gemini. It has too many "L"s.
     """
@@ -38,15 +41,19 @@ st.markdown(
 st.image("./assets/fake_company.png")
 
 with st.expander("Company Info"):
-    st.write("""
+    st.write(
+        """
         Fake Company LLC Inc. is located at 1600 Amphitheatre Parkway Mountain View, CA 94043
-    """)
+    """
+    )
 
 with st.expander("Links"):
-    st.markdown("""
+    st.markdown(
+        """
         [Google](https://google.com)
 
         [Gemini](https://gemini.google.com)
 
         [Streamlit Docs](https://docs.streamlit.io/)
-    """)
+    """
+    )
--- pages/overview.py
+++ pages/overview.py
@@ -3,11 +3,14 @@ import streamlit as st
 if st.session_state.get("logged_in") == None:
     st.session_state["logged_in"] = False
 
+
 def login():
     st.session_state.logged_in = True
 
+
 def logout():
     st.session_state.logged_in = False
+
 
 st.set_page_config(page_title="Overview")
 
@@ -25,22 +28,27 @@ st.sidebar.write("This site is copyright Fake Company LLC Inc., 2024")
 
 st.write(
     """Here is a page with a site overview.
-    
+
     This site has one main page (app) and three pages (about, overview, and report).
 
     All of them have some redundant code that can be abstracted out to make changes easier in the future.
-    """)
+    """
+)
 
 with st.expander("Company Info"):
-    st.write("""
+    st.write(
+        """
         Fake Company LLC Inc. is located at 1600 Amphitheatre Parkway Mountain View, CA 94043
-    """)
+    """
+    )
 
 with st.expander("Links"):
-    st.markdown("""
+    st.markdown(
+        """
         [Google](https://google.com)
 
         [Gemini](https://gemini.google.com)
-        
+
         [Streamlit Docs](https://docs.streamlit.io/)
-    """)
+    """
+    )
